3c299152d846292ec745489c3b6d2122c0c2f72f
saulpalv <19735362+saulpalv@users.noreply.github.com>
2022-08-06 14:56:20 -0600

    first commit

---
+++ Program.fs
@@ -0,0 +1,54 @@
+﻿namespace gtk3SkiaFS
+
+open Gtk
+open SkiaSharp
+open SkiaSharp.Views.Gtk
+open System
+
+module Program =
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        Application.Init()
+
+        let app = new Application("org.gtk3SkiaFS.gtk3SkiaFS", GLib.ApplicationFlags.None)
+        app.Register(GLib.Cancellable.Current) |> ignore
+
+        let win = new Window(WindowType.Toplevel)
+
+        win.Title <- "SkiaSharp on .NET Core with GTK#"
+        win.WindowPosition <- WindowPosition.CenterOnParent
+        win.DefaultWidth <- 640
+        win.DefaultHeight <- 480
+
+        win.DeleteEvent.Add(fun e -> Application.Quit())
+
+        let skiaDraw = new SKDrawingArea()
+
+        skiaDraw.PaintSurface.Add (fun e ->
+            let canvas = e.Surface.Canvas
+            let info = e.Info
+
+            canvas.Clear(SKColors.Azure)
+
+            let paint =
+                new SKPaint(
+                    Color = SKColors.Orange,
+                    IsAntialias = true,
+                    Style = SKPaintStyle.Fill,
+                    TextAlign = SKTextAlign.Center,
+                    TextSize = 32f
+                )
+
+            let skPoint =
+                SKPoint(x = float32 (info.Width / 2), y = float32 ((info.Height + int (paint.TextSize)) / 2))
+
+            canvas.DrawText("SkiaSharp on .NET Core with GTK#", skPoint, paint))
+
+        win.Add(skiaDraw)
+        win.Child.ShowAll()
+        win.Show()
+        app.AddWindow(win)
+        win.Show()
+        Application.Run()
+        0
---
+++ README.md
@@ -0,0 +1,3 @@
+Example of Skia surface on a GTK3 window using F#
+
+Make sure you have [GTK installed](https://github.com/GtkSharp/GtkSharp/wiki/Installing-Gtk-on-Mac)
